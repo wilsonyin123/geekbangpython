17c8d23ca864b7cb1e9269e7110e55dd4db0df0f
hanhan <hanhan_smith@hotmail.com>
2018-06-03 16:38:23 +0800

    完成变量作业

---
+++ exercise/3变量/variable.py
@@ -0,0 +1,14 @@
+
+# # 练习一 变量的定义和使用
+#
+# 1. 定义两个变量分别为美元和汇率
+# 2. 通过搜索引擎找到美元兑人民币汇率
+# 3. 使用Python计算100美元兑换的人民币数量并用print( )进行输出
+
+dollar = 100.00
+exchange_rate = 6.42
+rmb = dollar * exchange_rate
+
+
+
+print("%f 美元兑换的人民币数量= %f " %(dollar,rmb))
