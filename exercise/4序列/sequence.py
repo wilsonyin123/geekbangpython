96be2f9ad4d1b5cc153804eb6fd41a3a0f4d255f
hanhan <hanhan_smith@hotmail.com>
2018-06-03 17:07:46 +0800

    完成第一个练习

--- exercise/4序列/sequence.py
+++ exercise/4序列/sequence.py
@@ -1,5 +1,22 @@
-# 练习一 字符串
+# # 练习一 字符串
+#
+# 1. 定义一个字符串Hello Python 并使用print( )输出
 
-1. 定义一个字符串Hello Python 并使用print( )输出
-2. 定义第二个字符串Let‘s go并使用print( )输出
-3. 定义第三个字符串"The Zen of Python" -- by Tim Peters 并使用print( )输出
+str1 = 'Hello Python!'
+
+print(str1)
+
+# 2. 定义第二个字符串Let‘s go并使用print( )输出
+
+str2 = "Let's go"
+
+print(str2)
+
+# 3. 定义第三个字符串"The Zen of Python" -- by Tim Peters 并使用print( )输出
+
+str3 = " \"The Zen of Python\" -- by Tim Peters"
+
+print(str3)
+
+
+
